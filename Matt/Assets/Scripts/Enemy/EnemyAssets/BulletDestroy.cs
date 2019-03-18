9f28e97095a49cf9bcdc5fb3246c80ea64dd05ae
Tytoowns <tytoowns@gmail.com>
2019-03-18 19:54:18 +0000

    Changed BulletDestroy.cs
    Changed it to be more efficient. Also stops player from getting hit multiple times.

--- Matt/Assets/Scripts/Enemy/EnemyAssets/BulletDestroy.cs
+++ Matt/Assets/Scripts/Enemy/EnemyAssets/BulletDestroy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletDestroy : MonoBehaviour
-{
+{/*
     public Transform bullet;
     public float colissionRadius = 0.4f;
     public bool collided = false;
@@ -16,5 +16,15 @@ public class BulletDestroy : MonoBehaviour
         if (collided) Destroy(gameObject);
         if (!GetComponent<Renderer>().isVisible) Destroy(gameObject);
         
+    }
+    */
+    void OnTriggerStay2D(Collider2D col) //On collision with an object with the EnemyAttack tag it will reduce the hp of the player.
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Player") || col.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (!GetComponent<Renderer>().isVisible) Destroy(gameObject);
     }
 }
