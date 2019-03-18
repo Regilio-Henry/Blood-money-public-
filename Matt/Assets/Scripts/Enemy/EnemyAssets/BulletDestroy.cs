b9ebc0bf082f06f3572617a7de2ab53412bc37f4
Tytoowns <tytoowns@gmail.com>
2019-03-18 20:01:10 +0000

    BulletDestroy Update
    Added the fixedupdate back to check if the spiderweb projectile is still visable

--- Matt/Assets/Scripts/Enemy/EnemyAssets/BulletDestroy.cs
+++ Matt/Assets/Scripts/Enemy/EnemyAssets/BulletDestroy.cs
@@ -3,28 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletDestroy : MonoBehaviour
-{/*
-    public Transform bullet;
-    public float colissionRadius = 0.4f;
-    public bool collided = false;
-    public LayerMask whatToCollideWith;
-
+{
     void FixedUpdate()
     {
-        collided = Physics2D.OverlapCircle(bullet.position, colissionRadius, whatToCollideWith); 
-
-        if (collided) Destroy(gameObject);
-        if (!GetComponent<Renderer>().isVisible) Destroy(gameObject);
-        
+        if (!GetComponent<Renderer>().isVisible)
+        {
+            Destroy(gameObject);
+        }
     }
-    */
-    void OnTriggerStay2D(Collider2D col) //On collision with an object with the EnemyAttack tag it will reduce the hp of the player.
+
+    void OnTriggerStay2D(Collider2D col)
     {
         if (col.gameObject.layer == LayerMask.NameToLayer("Player") || col.gameObject.layer == LayerMask.NameToLayer("Wall"))
         {
             Destroy(gameObject);
         }
-
-        if (!GetComponent<Renderer>().isVisible) Destroy(gameObject);
     }
 }
