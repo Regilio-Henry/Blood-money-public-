66be578d9c3c019e9683f3888b4a0679a9feda22
Tytoowns <tytoowns@gmail.com>
2019-04-06 18:47:05 +0100

    Added music to the main menu
    Added music to the main menu that will fade in slowly

---
+++ Matt/Assets/Scripts/BackGroundMusicFadeIn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class BackGroundMusicFadeIn : MonoBehaviour
+{
+    [SerializeField]
+    private int m_FadeInTime = 10;
+    private AudioSource m_AudioSource;
+
+
+    private void Awake()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+
+    private void Update()
+    {
+        if (m_AudioSource.volume < .5)
+        {
+            m_AudioSource.volume = m_AudioSource.volume + (Time.deltaTime / (m_FadeInTime + 1));
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
